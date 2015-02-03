ad7baac2dc8eeb2209bc3caa434898f6cb05d7fc
Kyle Harrison <kylejharrison@gmail.com>
2015-02-03 14:47:11 +0000

    refactored MainMenu

--- src/main/java/game/elements/MainMenu.java
+++ src/main/java/game/elements/MainMenu.java
@@ -10,24 +10,52 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class MainMenu extends JPanel{
-    public static void main(String[] args){
-        //Create the main game window
-        JFrame frame = new JFrame("Havannah Main Menu");
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e){
-                System.exit(0);
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        // Stolen from http://docs.oracle.com/javase/tutorial/displayCode.html?code=http://docs.oracle.com/javase/tutorial/uiswing/examples/components/FrameDemoProject/src/components/FrameDemo.java
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame window = createUI();
+                addElements(window);
+                showUI(window);
             }
         });
+    }
+
+    private static JFrame createUI() {
+        //Create the main game window
+        JFrame frame = new JFrame("Havannah Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Container container = frame.getContentPane();
         frame.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+        //Set size and make the window visible
+        frame.setSize(450, 350);
+
+        return frame;
+    }
+
+    private static void showUI(JFrame frame) {
+        frame.setVisible(true);
+    }
+
+    private static void addElements(JFrame frame) {
+        //add elements to the window in order
+        addTitle(frame);
+        addNewGameButton(frame);
+    }
+
+    private static void addTitle(JFrame frame) {
         //Add the title text at the top
         Title title = new Title("Welcome to Havannah");
         frame.add(title);
+    }
 
+    private static void addNewGameButton(JFrame frame){
         //Add a button to start new game.elements.Game
         ui.Button newGame = new ui.Button("Start New Game");
         newGame.addActionListener(new ActionListener() {
@@ -44,9 +72,5 @@ public class MainMenu extends JPanel{
             }
         });
         frame.add(newGame);
-
-        //Set size and make the window visible
-        frame.setSize(450, 350);
-        frame.setVisible(true);
     }
 }
