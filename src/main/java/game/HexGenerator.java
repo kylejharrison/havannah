0396fae72e4306499c10603fa482731f978c4299
Kyle Harrison <kylejharrison@gmail.com>
2015-02-08 18:36:12 +0000

    HexGenerator now creates edges and corners successfully and is fully tested

--- src/main/java/game/HexGenerator.java
+++ src/main/java/game/HexGenerator.java
@@ -34,29 +34,58 @@ public class HexGenerator {
 
     private void generateHexesGoingUpRight(int x){
         for (int y = 0; y < boardSize; y++) {
-            Corner corner = Corner.NOTACORNER;
-            if (x == 0 && y == boardSize -1 ){
-                corner = Corner.TOP;
-            }else if (x == boardSize -1 && y == boardSize - 1){
-                corner = Corner.TOPRIGHT;
-            }else if (x == boardSize -1 && y == 0){
-                corner = Corner.BOTTOMRIGHT;
-            }
-            final HexImpl hex = new HexImpl(x, -y, Edge.NOTANEDGE, corner);
+            Corner corner = getGoingUpRightCorner(x, y);
+            Edge edge = getGoingUpRightEdge(x, y);
+            final HexImpl hex = new HexImpl(x, -y, edge, corner);
             allHexes.add(hex);
         }
     }
 
+    private Corner getGoingUpRightCorner(int x, int y){
+        Corner corner = Corner.NOTACORNER;
+        if (x == 0 && y == boardSize -1 ){
+            corner = Corner.TOP;
+        }else if (x == boardSize -1 && y == boardSize - 1){
+            corner = Corner.TOPRIGHT;
+        }else if (x == boardSize -1 && y == 0){
+            corner = Corner.BOTTOMRIGHT;
+        }
+        return corner;
+    }
+    private Edge getGoingUpRightEdge(int x, int y){
+        Edge edge = Edge.NOTANEDGE;
+        if (y == boardSize - 1 && x > 0 && x < boardSize -1){
+            edge = Edge.TOPRIGHT;
+        } else if (x == boardSize -1 && y > 0 && y < boardSize -1){
+            edge = Edge.RIGHT;
+        }
+        return edge;
+    }
     private void generateHexesGoingDownRight(int x){
         for (int y = 0; y < boardSize -1 -x; y++) {
-            Corner corner = Corner.NOTACORNER;
-            if (x == 0 && y == boardSize - 2 ) {
-                corner = Corner.BOTTOM;
-            }
-            final HexImpl hex = new HexImpl(x, y + 1, Edge.NOTANEDGE, corner);
+            Corner corner = getGoingDownRightCorner(x, y);
+            Edge edge = getGoingDownRightEdge(x, y);
+            final HexImpl hex = new HexImpl(x, y + 1, edge, corner);
             allHexes.add(hex);
         }
     }
+
+    private Corner getGoingDownRightCorner(int x, int y){
+        Corner corner = Corner.NOTACORNER;
+        if (x == 0 && y == boardSize - 2 ) {
+            corner = Corner.BOTTOM;
+        }
+        return corner;
+    }
+
+    private Edge getGoingDownRightEdge(int x, int y){
+        Edge edge = Edge.NOTANEDGE;
+        if (y == boardSize -2 -x && x > 0 && x < boardSize -1){
+            edge = Edge.BOTTOMRIGHT;
+        }
+        return edge;
+    }
+
     private void generateHexesToTheLeft() {
         for (int x = 0; x < boardSize - 1; x++) {
             //draw tiles upwards
@@ -67,24 +96,53 @@ public class HexGenerator {
 
     private void generateHexesGoingUpLeft(int x){
         for (int y = 0; y < boardSize - 1 - x; y++) {
-            Corner corner = Corner.NOTACORNER;
-            if (x == boardSize - 2 && y == 0 ){
-                corner = Corner.TOPLEFT;
-            }
-            final HexImpl hex = new HexImpl(-x - 1, -y, Edge.NOTANEDGE, corner);
+            Corner corner = getGoingUpLeftCorner(x, y);
+            Edge edge = getGoingUpLeftEdge(x, y);
+            final HexImpl hex = new HexImpl(-x - 1, -y, edge, corner);
             allHexes.add(hex);
         }
     }
 
+    private Corner getGoingUpLeftCorner(int x, int y){
+        Corner corner = Corner.NOTACORNER;
+        if (x == boardSize - 2 && y == 0 ){
+            corner = Corner.TOPLEFT;
+        }
+        return corner;
+    }
+
+    private Edge getGoingUpLeftEdge(int x, int y){
+        Edge edge = Edge.NOTANEDGE;
+        if (y == boardSize -2 - x && x < boardSize -2 ){
+            edge = Edge.TOPLEFT;
+        }
+        return edge;
+    }
     private void generateHexesGoingDownLeft(int x){
         for (int y = 0; y < boardSize - 1; y++) {
-            Corner corner = Corner.NOTACORNER;
-            if (x == boardSize - 2 && y == boardSize - 2 ){
-                corner = Corner.BOTTOMLEFT;
-            }
-            final HexImpl hex = new HexImpl(-x - 1, y + 1, Edge.NOTANEDGE, corner);
+            Corner corner = getGoingDownLeftCorner(x, y);
+            Edge edge = getGoingDownLeftEdge(x, y);
+            final HexImpl hex = new HexImpl(-x - 1, y + 1, edge, corner);
             allHexes.add(hex);
         }
+    }
+
+    private Corner getGoingDownLeftCorner(int x, int y){
+        Corner corner = Corner.NOTACORNER;
+        if (x == boardSize - 2 && y == boardSize - 2 ){
+            corner = Corner.BOTTOMLEFT;
+        }
+        return corner;
+    }
+
+    private Edge getGoingDownLeftEdge(int x, int y){
+        Edge edge = Edge.NOTANEDGE;
+        if (y == boardSize - 2 && x < boardSize -2){
+            edge = Edge.BOTTOMLEFT;
+        } else if (x == boardSize - 2 && y < boardSize - 2){
+            edge = Edge.LEFT;
+        }
+        return edge;
     }
 
 }
--- src/test/java/game/HexGeneratorTest.java
+++ src/test/java/game/HexGeneratorTest.java
@@ -1,16 +1,17 @@
 package game;
 
 import game.elements.Corner;
+import game.elements.Edge;
 import game.elements.Hex;
 import game.elements.HexImpl;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class HexGeneratorTest {
 
@@ -52,33 +53,227 @@ public class HexGeneratorTest {
     @Test
     public void testHexCornersAreCreatedCorrectly() throws Exception{
         for (int boardSize = 2; boardSize < 20; boardSize++) {
+            Map<HexImpl,HexImpl> hexMap = getHexMap(boardSize);
+            assertAllCorners(hexMap, boardSize);
+            assertRemainingHexesAreNotACorner(hexMap);
+        }
+    }
+    @Test
+    public void testHexEdgesAreCreatedCorrectly() throws Exception{
+        for (int boardSize = 2; boardSize < 20; boardSize++) {
+            Map<HexImpl,HexImpl> hexMap = getHexMap(boardSize);
+            assertAllEdges(hexMap,boardSize);
+            assertRemainingHexesAreNotAnEdge(hexMap);
+        }
+    }
+
+    @Test
+    public void testCorrectNumberOfCorners() throws Exception{
+        for (int boardSize = 2; boardSize < 20; boardSize++) {
             HexGenerator hexGenerator = new HexGenerator(boardSize);
             Set<HexImpl> allHexes = hexGenerator.generateHexes();
-            Map<HexImpl, HexImpl> hexMap = new HashMap<HexImpl, HexImpl>();
+            int numCorners = 0;
             for (HexImpl hex: allHexes){
-                hexMap.put(hex,hex);
+                if (hex.getCorner().isACorner()) numCorners++;
             }
-            HexImpl top = new HexImpl(0, 1 - boardSize);
-            allHexes.remove(top);
-            HexImpl topRight = new HexImpl(boardSize - 1, 1 - boardSize);
-            allHexes.remove(topRight);
-            HexImpl bottomRight = new HexImpl(boardSize - 1, 0);
-            allHexes.remove(bottomRight);
-            HexImpl bottom = new HexImpl(0, boardSize - 1);
-            allHexes.remove(bottom);
-            HexImpl bottomLeft = new HexImpl(1 - boardSize, boardSize - 1);
-            allHexes.remove(bottomLeft);
-            HexImpl topLeft = new HexImpl(1 - boardSize, 0);
-            allHexes.remove(topLeft);
-            assertEquals(hexMap.get(top).getCorner(), Corner.TOP);
-            assertEquals(hexMap.get(topRight).getCorner(), Corner.TOPRIGHT);
-            assertEquals(hexMap.get(bottomRight).getCorner(), Corner.BOTTOMRIGHT);
-            assertEquals(hexMap.get(bottom).getCorner(), Corner.BOTTOM);
-            assertEquals(hexMap.get(bottomLeft).getCorner(), Corner.BOTTOMLEFT);
-            assertEquals(hexMap.get(topLeft).getCorner(), Corner.TOPLEFT);
+            assertEquals(numCorners,6);
+        }
+    }
+
+    @Test
+    public void testAllCornersAreUnique() throws Exception{
+        for (int boardSize = 2; boardSize < 20; boardSize++) {
+            HexGenerator hexGenerator = new HexGenerator(boardSize);
+            Set<HexImpl> allHexes = hexGenerator.generateHexes();
+            Set<Corner> allCorners = new HashSet<Corner>();
             for (HexImpl hex: allHexes){
-                assertEquals(hex.getCorner(),Corner.NOTACORNER);
+                if (hex.getCorner().isACorner()){
+                    assertFalse(allCorners.contains(hex.getCorner()));
+                    allCorners.add(hex.getCorner());
+                }
             }
         }
+    }
+
+    @Test
+    public void testCorrectNumberOfEdges() throws Exception{
+        for (int boardSize = 2; boardSize < 20; boardSize++) {
+            HexGenerator hexGenerator = new HexGenerator(boardSize);
+            Set<HexImpl> allHexes = hexGenerator.generateHexes();
+            int numEdges = 0;
+            for (HexImpl hex: allHexes){
+                if (hex.getEdge().isAnEdge()) numEdges++;
+            }
+            assertEquals(numEdges, 6 * (boardSize - 2));
+        }
+    }
+
+    @Test
+    public void testNoHexesAreBothACornerAndAnEdge() throws Exception{
+        for (int boardSize = 2; boardSize < 20; boardSize++) {
+            HexGenerator hexGenerator = new HexGenerator(boardSize);
+            Set<HexImpl> allHexes = hexGenerator.generateHexes();
+            for (HexImpl hex: allHexes){
+                assertFalse(hex.getCorner().isACorner() && hex.getEdge().isAnEdge());
+            }
+        }
+    }
+
+    private void assertAllEdges(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        assertTopRightEdge(hexMap, boardSize);
+        assertRightEdge(hexMap,boardSize);
+        assertBottomRightEdge(hexMap,boardSize);
+        assertBottomLeftEdge(hexMap,boardSize);
+        assertLeftEdge(hexMap,boardSize);
+        assertTopLeftEdge(hexMap,boardSize);
+    }
+    private void assertRemainingHexesAreNotAnEdge(Map<HexImpl,HexImpl> hexMap){
+        for (Map.Entry<HexImpl,HexImpl> hex: hexMap.entrySet()) {
+            assertEquals(hex.getValue().getEdge(), Edge.NOTANEDGE,String.format("AllHexes: %s, Hex: %s",hexMap,hex.getValue().toString()));
+        }
+    }
+    private void assertTopRightEdge(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        Set<HexImpl> topRightHexes = getTopRightEdges(boardSize);
+        for (HexImpl hex: topRightHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.TOPRIGHT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getTopRightEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        for (int x = 1; x < boardSize -1; x++) {
+            hexes.add(new HexImpl(x,1 - boardSize));
+        }
+        return hexes;
+    }
+    private void assertRightEdge(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        Set<HexImpl> RightHexes = getRightEdges(boardSize);
+        for (HexImpl hex: RightHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.RIGHT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getRightEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        for (int y = 2 - boardSize; y < 0; y++) {
+            hexes.add(new HexImpl(boardSize - 1,y));
+        }
+        return hexes;
+    }
+    private void assertBottomRightEdge(Map<HexImpl, HexImpl> hexMap, int boardSize){
+        Set<HexImpl> BottomRightHexes = getBottomRightEdges(boardSize);
+        for (HexImpl hex: BottomRightHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.BOTTOMRIGHT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getBottomRightEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        int y = boardSize - 2;
+        for (int x = 1; x < boardSize -1; x++) {
+            hexes.add(new HexImpl(x,y));
+            y--;
+        }
+        return hexes;
+    }
+
+    private void assertBottomLeftEdge(Map<HexImpl, HexImpl> hexMap, int boardSize){
+        Set<HexImpl> BottomLeftHexes = getBottomLeftEdges(boardSize);
+        for (HexImpl hex: BottomLeftHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.BOTTOMLEFT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getBottomLeftEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        for (int x = 2 - boardSize; x < 0; x++) {
+            hexes.add(new HexImpl(x,boardSize -1));
+        }
+        return hexes;
+    }
+
+    private void assertLeftEdge(Map<HexImpl, HexImpl> hexMap, int boardSize){
+        Set<HexImpl> LeftHexes = getLeftEdges(boardSize);
+        for (HexImpl hex: LeftHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.LEFT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getLeftEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        for (int y = 1 ; y < boardSize - 1; y++) {
+            hexes.add(new HexImpl(1 - boardSize,y));
+        }
+        return hexes;
+    }
+
+    private void assertTopLeftEdge(Map<HexImpl, HexImpl> hexMap, int boardSize){
+        Set<HexImpl> TopLeftHexes = getTopLeftEdges(boardSize);
+        for (HexImpl hex: TopLeftHexes){
+            assertEquals(hexMap.get(hex).getEdge(), Edge.TOPLEFT);
+            hexMap.remove(hex);
+        }
+    }
+    private Set<HexImpl> getTopLeftEdges(int boardSize){
+        Set<HexImpl> hexes = new HashSet<HexImpl>();
+        int y = -1;
+        for (int x = 2 - boardSize ; x < 0; x++) {
+            hexes.add(new HexImpl(x,y));
+            y--;
+        }
+        return hexes;
+    }
+
+    private void assertAllCorners(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        assertTopCorner(hexMap, boardSize);
+        assertTopRightCorner(hexMap, boardSize);
+        assertBottomRightCorner(hexMap,boardSize);
+        assertBottomCorner(hexMap,boardSize);
+        assertBottomLeftCorner(hexMap,boardSize);
+        assertTopLeftCorner(hexMap,boardSize);
+    }
+    private void assertRemainingHexesAreNotACorner(Map<HexImpl,HexImpl> hexMap){
+        for (Map.Entry<HexImpl,HexImpl> hex: hexMap.entrySet()) {
+            assertEquals(hex.getValue().getCorner(), Corner.NOTACORNER);
+        }
+    }
+    private void assertTopCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl top = new HexImpl(0, 1 - boardSize);
+        assertEquals(hexMap.get(top).getCorner(), Corner.TOP);
+        hexMap.remove(top);
+    }
+    private void assertTopRightCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl topRight = new HexImpl(boardSize - 1, 1 - boardSize);
+        assertEquals(hexMap.get(topRight).getCorner(), Corner.TOPRIGHT);
+        hexMap.remove(topRight);
+    }
+    private void assertBottomRightCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl bottomRight = new HexImpl(boardSize - 1, 0);
+        assertEquals(hexMap.get(bottomRight).getCorner(), Corner.BOTTOMRIGHT);
+        hexMap.remove(bottomRight);
+    }
+    private void assertBottomCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl bottom = new HexImpl(0, boardSize - 1);
+        assertEquals(hexMap.get(bottom).getCorner(), Corner.BOTTOM);
+        hexMap.remove(bottom);
+    }
+    private void assertBottomLeftCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl bottomLeft = new HexImpl(1 - boardSize, boardSize - 1);
+        assertEquals(hexMap.get(bottomLeft).getCorner(), Corner.BOTTOMLEFT);
+        hexMap.remove(bottomLeft);
+    }
+    private void assertTopLeftCorner(Map<HexImpl,HexImpl> hexMap, int boardSize){
+        HexImpl topLeft = new HexImpl(1 - boardSize, 0);
+        assertEquals(hexMap.get(topLeft).getCorner(), Corner.TOPLEFT);
+        hexMap.remove(topLeft);
+    }
+    private static Map<HexImpl,HexImpl> getHexMap(int boardSize){
+        HexGenerator hexGenerator = new HexGenerator(boardSize);
+        Set<HexImpl> allHexes = hexGenerator.generateHexes();
+        Map<HexImpl, HexImpl> hexMap = new HashMap<HexImpl, HexImpl>();
+        for (HexImpl hex: allHexes){
+            hexMap.put(hex,hex);
+        }
+        return hexMap;
     }
 }
